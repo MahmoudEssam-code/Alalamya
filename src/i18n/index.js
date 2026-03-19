d2d94c708d0bf98fcbf8c3b5062377c21cde6439
vanit <mahmoudessam.3040@gmail.com>
2026-03-19 21:52:43 +0200

    Update index.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -189,10 +189,6 @@ i18n
   .init({
     resources,
     fallbackLng: 'ar',
-    detection: {
-      order: ['localStorage', 'sessionStorage', 'cookie', 'htmlTag', 'path', 'subdomain'],
-      caches: ['localStorage', 'cookie']
-    },
     interpolation: {
       escapeValue: false
     }
